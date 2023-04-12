44323463e13918ac62f7d93fcb11d6adf1c9cbb7
emdoem <125324546+emdoem@users.noreply.github.com>
2023-04-12 19:42:00 +0200

    using CSS variables and props to alter styling

--- src/App.js
+++ src/App.js
@@ -2,24 +2,36 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  // const buttonStyle = { backgroundColor: "red" };
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Button type="danger">Click Me</Button>
+        <PrimaryButton>Click Me</PrimaryButton>
+        <Button>Click Me</Button>
       </header>
     </div>
   );
+}
+
+
+function Button(props) {
+  const buttonStyle = {}
+  if (props.type === "primary") {
+    buttonStyle["--normal-background"] = "blue";
+    buttonStyle["--hover-background"] = "darkblue";
+    buttonStyle["--active-background"] = "lightblue";
+  } else if (props.type === "danger") {
+    buttonStyle["--normal-background"] = "red";
+    buttonStyle["--hover-background"] = "darkred";
+    buttonStyle["--active-background"] = "pink";
+  }
+  buttonStyle["--border-radius"] = "12px"
+  return <button className="Button" style={buttonStyle}>{props.children}</button>
+}
+
+function PrimaryButton(props) {
+  return <Button type="primary">{props.children}</Button>
 }
 
 export default App;
